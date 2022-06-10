fca5b05cdd93a908e07c1bd4dd58e0d2f8e6ef78
igh111 <58137504+igh111@users.noreply.github.com>
2022-06-10 16:10:36 +0900

    Commit

---
+++ Assets/ASDFASDFASDFASDFASDF.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASDFASDFASDFASDFASDF : MonoBehaviour
+{
+    private void Method()
+    {
+
+    }
+}
